21abfac4384ec8659d5087c4decf5011781794ef
nadjagv <58389979+nadjagv@users.noreply.github.com>
2024-05-28 23:15:02 +0200

    feat: Initialise spring project. (#1)

---
+++ src/test/java/ftn/devops/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package ftn.devops.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
